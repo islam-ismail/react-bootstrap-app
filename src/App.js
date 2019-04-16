e6722e15f0ff5b418dc4413bebc3267f3ccd2f00
eslam elaraby <islam.ismail038@gmail.com>
2019-04-16 12:21:57 +0200

    react-router-dom and react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
-import './App.scss';
-import { BrowserRouter , Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import News from './components/News';
-import Navbar from './components/CustomNavbar';
-
+import React, { Component } from "react";
+import "./App.scss";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import News from "./components/News";
+import Navbar from "./components/CustomNavbar";
 
 class App extends Component {
   render() {
     return (
-     <BrowserRouter>
-     <div>
-       <Navbar />
-       <Route exact path ='/' component={Home} />
-       <Route path ='/about' component={About} />
-       <Route path ='/news' component={News} />
-     </div>
-     </BrowserRouter>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/news" component={News} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
-import { Navbar , Nav , Form , FormControl , Button} from 'react-bootstrap';
-import './CustomNavbar.scss';
+import React, { Component } from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import "./CustomNavbar.scss";
 
 class CustomNavbar extends Component {
-    render() {
-        return (
-        <Navbar bg="dark" variant="dark" collapseOnSelect >
-             <Navbar.Brand href="/">
-                React App
-             </Navbar.Brand>
-             <Navbar.Toggle />
-             <Navbar.Collapse >
-               <Nav className="justify-content-end">
-                 <Nav.Link href="/">Home</Nav.Link>
-                 <Nav.Link href="about">About</Nav.Link>
-                 <Nav.Link href="news">News</Nav.Link>
-               </Nav>
-             </Navbar.Collapse>
-             <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-info">Search</Button>
-             </Form>
-        
-        </Navbar>
-        );
-    }
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark" collapseOnSelect>
+        <Navbar.Brand href="/">React App</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="justify-content-end">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="news">News</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+    );
+  }
 }
 
 export default CustomNavbar;
